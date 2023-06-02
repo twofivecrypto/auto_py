03227cc99fd95b85db5cb1d4f283fe9f47db408a
Mansa <twofivecrypto@gmail.com>
2023-06-01 22:23:58 -0400

    tictacs

--- chp5_dictionaries&structuring_data/characterCount.py
+++ chp5_dictionaries&structuring_data/characterCount.py
@@ -1,7 +1,19 @@
+import pprint
+
 message = 'I am the descendant of the last Mansa, Mansaba Janke Waali Sanneh. I have come to claim my title as the Mansabra.'
 count = {}
 
 for character in message:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
-print(count)
+pprint.pprint(count)
+
+'''
+GOOD TO KNOW -- PPRINT FORMATS
+
+
+pprint.pprint(someDictionaryValue)
+print('\n')
+pprint(pprint.pformat(someDictionaryValue))
+
+'''
