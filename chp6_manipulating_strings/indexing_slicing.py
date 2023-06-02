6454f59357da3ff212767b784ef3d66b267ded74
Mansa <twofivecrypto@gmail.com>
2023-06-02 01:51:31 -0400

    pg130

---
+++ chp6_manipulating_strings/indexing_slicing.py
@@ -0,0 +1,13 @@
+spam = 'Hello world!'
+print(spam[:10])
+print(spam[6:])
+print(spam[:6])
+
+fizz = spam[:6]
+rizz = spam[0:5]
+tizz = spam[:4]
+
+print(fizz + ' ' + rizz)
+print('Don\'t go to ' + tizz.title())
+
+
---
+++ chp6_manipulating_strings/strings_one.py
@@ -0,0 +1,10 @@
+spam = "That is Alice's cat."
+
+#USING ESCAPE CHARACTERS
+spam = 'Say hi to Bob\'s mother.'
+#use \' or \"  as escape characters to let python know not to end the string value.
+
+#RAW STRINGS
+#PLACE AN 'r' before the beginning quotation mark to make a raw string. 
+print(r'That is Carol\'s cat.')
+
