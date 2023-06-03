a9dd28380e7bf26fbd222131dacb0ea7f9613d2a
Mansa_Forever <94782619+twofivecrypto@users.noreply.github.com>
2023-06-02 23:27:56 -0400

    bpupload

---
+++ bulletPointMarkup.py
@@ -0,0 +1,13 @@
+#! python3
+#bulletPoint.py - Adds Wikipedia bullet points to the start of each line of text in the clipboard
+
+import pyperclip
+
+text = pyperclip.paste()
+
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
