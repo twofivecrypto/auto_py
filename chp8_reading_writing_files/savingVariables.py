945f8d3013a2443e598a8b7cc66e7e587f75b5c1
Mansa <twofivecrypto@gmail.com>
2023-06-08 01:15:07 -0400

    p185

--- chp8_reading_writing_files/savingVariables.py
+++ chp8_reading_writing_files/savingVariables.py
@@ -3,8 +3,14 @@ import shelve
 shelfFile = shelve.open('mydata')
 cats = ['Zophie', 'Pooka', 'Simon']
 shelfFile['cats'] = cats
-print(shelfFile.close())
+shelfFile.close()
 
-shelfFile - shelve.open('mydata')
+shelfFile = shelve.open('mydata')
 type(shelfFile)
-print(shelfFile['cats'])
+print(shelfFile['cats'])#['Zophie', 'Pooka', 'Simon']
+
+#GETTING THE LIST FORM WITH list()
+shelfFile = shelve.open('mydata')
+print(list(shelfFile.keys()))#['cats']
+print(list(shelfFile.values()))#[['Zophie', 'Pooka', 'Simon']]
+shelfFile.close()
