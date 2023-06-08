455a3d069774e6635eb26d0b6eb19e75393aa86b
Mansa <twofivecrypto@gmail.com>
2023-06-08 01:54:20 -0400

    P188 - 1/2 there

---
+++ projects/Generating Random Quiz Files/randomQuiz.py
@@ -0,0 +1,70 @@
+#! python3
+#randomQuiz.py - Creates quizzes with random ordered questions with their answers.
+
+import random
+
+capitals = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Pheonix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Conneticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Moines',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfurt',
+    'Lousiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachuseets': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'Saint Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersy': 'Trenton',
+    'New Mexixo': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismark',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montepelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'
+}
+
+#GENERATE 35 QUIZ FILES
+
+for quizNum in range(35):
+    
+    #TODO: CREATE THE QUIZ AND ANSWER KEY FILES
+    
+    # TODO: WRITE OUT THE HEADER FOR THE QUIZ
+    
+    #TODO: SHUFFLE THE ODER OF TEH STATES
+    
+    #TODO: LOOP THROUGH ALL 50 STATES, MAKING A QUESTION FOR EACH
+    
