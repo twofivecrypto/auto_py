86cbea26aec06ec854c4b6afd9f757fa4a8678c0
Mansa <twofivecrypto@gmail.com>
2023-06-13 00:55:54 -0400

    p204, must do

--- chp9_organizing_files/zippedFiles.py
+++ chp9_organizing_files/zippedFiles.py
@@ -1,4 +1,5 @@
 import zipfile, os
-os.chdir('D:\Web-Dev-For-Beginners-main.zip') #MOVE TO THE FOLDER WITH example.zip
+os.chdir("D:\Web-Dev-For-Beginners-main.zip") #MOVE TO THE FOLDER WITH example.zip
 exampleZip = zipFile.ZipFile('example.zip')
 exampleZip.namelist()
+
