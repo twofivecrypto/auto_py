e190aad20051b5d11f8e3286055a80e02a2f5922
Mansa <twofivecrypto@gmail.com>
2023-06-12 22:01:42 -0400

    202

---
+++ chp9_organizing_files/letsGo.py
@@ -0,0 +1,12 @@
+import shutil, os
+
+#os.chdir('D:\\')
+
+# Copy a specific file from the source path to the destination path
+#shutil.copy2('D:\\auto_py\\ch1_basics\\index.html', 'D:\\auto_py\\chp9_organizing_files')
+
+# Copy and rename a specific file from the source path to the destination path
+#shutil.copy('D:\\auto_py\\chp9_organizing_files\\index.html', 'D:\\auto_py\\solo_p')
+
+shutil.move('D:\\auto_py\\solo_p\\template.html', 'D:\\auto_py\\chp9_organizing_files\\template2.html')
+
