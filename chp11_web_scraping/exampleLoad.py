6a0f5381aa2ad981e66570fc748fa9b33b813c55
Mansa <twofivecrypto@gmail.com>
2023-07-09 21:13:02 -0400

    pushed

--- chp11_web_scraping/exampleLoad.py
+++ chp11_web_scraping/exampleLoad.py
@@ -6,6 +6,7 @@ import bs4
 exampleFile = open("D:\\auto_py\\chp9_organizing_files\\index.html")
 exampleSoup = bs4.BeautifulSoup(exampleFile.read(), features="html.parser")
 elems = exampleSoup.select('#author')
+
 print(type(elems))
 print(len(elems))
 if len(elems) > 0:
@@ -14,3 +15,13 @@ if len(elems) > 0:
     print(str(elems[0]))
     print(elems[0].attrs)
 
+pElems = exampleSoup.select('p')
+if len(pElems) > 0:
+    print(str(pElems[0]))
+    print(pElems[0].getText())
+    if len(pElems) > 1:
+        print(str(pElems[1]))
+        print(pElems[1].getText())
+    if len(pElems) > 2:
+        print(str(pElems[2]))
+        print(pElems[2].getText())
